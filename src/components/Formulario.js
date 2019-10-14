668623de2801759373283e70dfd4df92f787909f
bgarcia95 <bgarcia.95@outlook.com>
2019-10-14 16:05:30 -0600

    Adding Formulario into our project

---
+++ src/components/Formulario.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+function Formulario() {
+    
+    const handleChange = e => {
+        // Cambiar el state
+    }
+    
+    return (
+        <form>
+            <div className="input-field col s12">
+                <input 
+                    type="text"
+                    name="ciudad"
+                    id="ciudad"
+                    onChange={handleChange}
+                 />
+                 <label htmlFor="ciudad" >Ciudad</label>
+            </div>
+            <div className="input-field col s12">
+                <select name="pais" onChange={handleChange}>
+                    <option value="">Selecciona un país</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="ES">El Salvador</option>
+                    <option value="MX">México</option>
+                    <option value="CO">Colombia</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="PE">Perú</option>
+                </select>
+            </div>
+
+            <div className="input-field col s12">
+                <input type="submit" className="waves-effect waves-light btn-large btn-block yellow accent-4" value="Buscar clima" />
+            </div>
+        </form>
+    );
+}
+
+export default Formulario;
