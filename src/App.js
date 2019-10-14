eb0386d7b96a746f3636abeab1d0a822a0a0b3ff
bgarcia95 <bgarcia.95@outlook.com>
2019-10-14 16:45:34 -0600

    Adding error  message if one of the fields  are missing

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import React, {useState} from "react";
 
 import Header from "./components/Header";
 import Formulario from "./components/Formulario";
-import { exportDefaultSpecifier } from "@babel/types";
+import Error from "./components/Error";
+
 
 function App() {
 
@@ -10,21 +11,33 @@ function App() {
   // ciudad = state, guardarCiudad = setState()
   const [ ciudad, guardarCiudad ] = useState('');
   const [ pais, guardarPais ] = useState('');
+  const [ error, guardarError] = useState(false);
 
   const datosConsulta = datos => {
     
     // Validar que ambos campos esten
     if(datos.ciudad === '' || datos.pais === '') {
       // un error
+      guardarError(true);
       return;
     }
 
     // Ciudad y pais  existen, agregarlos al state
     guardarCiudad(datos.ciudad);
     guardarPais(datos.pais);
+    guardarError(false);
   }
 
 
+  // Cargar un componente condicionalmente
+  let componente;
+  if(error) {
+    //Hay un error, mostrarlo
+    componente = <Error mensaje='Ambos campos son obligatorios' />
+  } else {
+    // Mostrar el clima
+    componente = null;
+  }
 
 
   return (
@@ -37,6 +50,9 @@ function App() {
             <div className="col s12 m6">
               <Formulario datosConsulta={datosConsulta} />
             </div>
+            <div className="col s12 m6">
+                {componente}
+              </div>
           </div>
         </div>
       </div>
